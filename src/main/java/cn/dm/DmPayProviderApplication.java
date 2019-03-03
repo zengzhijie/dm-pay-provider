4356cfa06737d5c036f906f6be4b3336264fee7e
曾志杰 <zengzhijie>
2019-03-03 14:33:17 +0800

    1

---
+++ src/main/java/cn/dm/DmPayProviderApplication.java
@@ -0,0 +1,13 @@
+package cn.dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmPayProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmPayProviderApplication.class, args);
+    }
+
+}
